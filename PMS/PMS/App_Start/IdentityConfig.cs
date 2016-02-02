aab13bd75afd748f44dd16990bdc1c0fd2629ed8
Ashwinss91 <ashwinss23@gmail.com>
2016-02-02 13:27:40 +0530

    Email login to UserName login

--- PMS/PMS/App_Start/IdentityConfig.cs
+++ PMS/PMS/App_Start/IdentityConfig.cs
@@ -46,7 +46,7 @@ namespace PMS
             // Configure validation logic for usernames
             manager.UserValidator = new UserValidator<ApplicationUser>(manager)
             {
-                AllowOnlyAlphanumericUserNames = false,
+                AllowOnlyAlphanumericUserNames = false,                
                 RequireUniqueEmail = true
             };
 
