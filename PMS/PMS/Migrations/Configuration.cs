39dc48ae81238d465b9896337e938fdb5b79095c
Ashwinss91 <ashwinss23@gmail.com>
2016-03-16 12:36:11 +0530

    Cleared data in database, Enable migrations and udpted database

--- PMS/PMS/Migrations/Configuration.cs
+++ PMS/PMS/Migrations/Configuration.cs
@@ -5,15 +5,14 @@ namespace PMS.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<PMS.Models.ApplicationDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<PMS.Models.DbModels.Entities>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = true;
-            
+            AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(PMS.Models.ApplicationDbContext context)
+        protected override void Seed(PMS.Models.DbModels.Entities context)
         {
             //  This method will be called after migrating to the latest version.
 
